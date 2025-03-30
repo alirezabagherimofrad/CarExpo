ec987d9f4ee949e869768b729ecf93c00b3db59d
hassanhbi <hbi1381819@gmail.com>
2025-03-30 12:51:49 +0330

    Generic Repository

---
+++ CarExpo.Infrastructure/Context/DataBaseContext.cs
@@ -0,0 +1,24 @@
+﻿using CarExpo.Domain.Models.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+public class DataBaseContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+{
+    public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+    {
+
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-BO3LEGK\\MSSQLSERVER01;Database=CarExpo;Trusted_Connection=True;TrustServerCertificate=True;", builder =>
+            {
+                builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+            });
+        }
+    }
+}
---
+++ CarExpo.Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿
+using CarExpo.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly DataBaseContext _context;
+
+    private readonly Dictionary<Type, object> _repositories = new();
+
+    public UnitOfWork(DataBaseContext context)
+    {
+        _context = context;
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+
+    public IGenericRepository<T> Repository<T>() where T : class
+    {
+        if (!_repositories.ContainsKey(typeof(T)))
+        {
+            var repository = new GenericRepository<T>(_context);
+            _repositories.Add(typeof(T), repository);
+        }
+        return (IGenericRepository<T>)_repositories[typeof(T)];
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+
+    }
+}
+
